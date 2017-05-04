edf00a028612f3f9356830ceceb23c4308490181
Adriana Ormaetxea <1adormae@gmail.com>
2017-05-04 13:41:35 +0200

    Add the locationCollectionCell

---
+++ BackbaseTestAdriana/ViewControllers/Cells/LocationCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  LocationCollectionViewCell.swift
+//  BackbaseTestAdriana
+//
+//  Created by Adriana Ormaetxea on 04/05/2017.
+//  Copyright © 2017 Mobiquity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocationsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel?
+    
+}
